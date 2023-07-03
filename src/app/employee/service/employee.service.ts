a76590bf8adb5dba343b426ba2a1379aa5a8d19a
WaghmodeSS <sachinwaghmode1000@gmail.com>
2023-07-03 19:56:22 +0530

    data added

--- src/app/employee/service/employee.service.ts
+++ src/app/employee/service/employee.service.ts
@@ -31,28 +31,28 @@ export class EmployeeService {
       "name": "James",
       "email": "james@gmail.com",
       "phone": 8584833432,
-      "address": "ABC",
+      "address": "Pune, India",
       "id": 1
     },
     {
       "name": "Clara",
       "email": "clara@gmail.com",
       "phone": 9983423854,
-      "address": "PQR",
+      "address": "Mumbai, India",
       "id": 2
     },
     {
       "name": "Wayne",
       "email": "wayne@gmail.com",
       "phone": 4348242792,
-      "address": "ABC",
+      "address": "Delhi, India",
       "id": 3
     },
     {
       "name": "Maya",
       "email": "maya@gmail.com",
       "phone": 9876543210,
-      "address": "ABC",
+      "address": "Pune, India",
       "id": 4
     }
   ];
